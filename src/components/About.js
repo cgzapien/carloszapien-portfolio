a712e5de7fbbf57c536cb1a525fb19df014200aa
cgzapien <65615888+cgzapien@users.noreply.github.com>
2022-01-03 16:49:59 -0700

    update

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <h1 className="text-5xl text-center" id="aboutme">About me</h1>
       <div className=" flex">
         <p className=" ">Hello, I'm Carlos and I am a Marine Corps Veteran looking to break into tech! Since leaving the militry I have been searching for a career that will challenge me as the military did. I feel like I've found it  </p>
-        <div className="bg-[url('/Users/zapien/Desktop/carloszapien_portfolio/src/media/male.jpg')] bg-cover p-60 rounded-[4px]" id="image"></div>
+        <div className=" bg-black bg-cover p-60 rounded-[4px]" id="image"></div>
 
       </div>
     </div>
